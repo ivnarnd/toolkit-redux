9d4caeb9761ecfd31c30062939fae776a110d6d9
Ivan Aranda <iaranda264@gmail.com>
2026-01-12 23:22:43 -0300

    configureStore y slices

---
+++ src/store/store.js
@@ -0,0 +1,7 @@
+import { configureStore} from "@reduxjs/toolkit";
+import { counterSlice } from "./slices/counter/index";
+export const store = configureStore({
+    reducer: {
+        counter : counterSlice.reducer
+    },
+});
